0ee92f6e10582a2a3c7e98f89fa01896c205515f
Robo <154452780+sumitkshatrya@users.noreply.github.com>
2026-09-12 22:22:50 +0530

    socket

--- admin/src/api/socketClient.js
+++ admin/src/api/socketClient.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = import.meta.env.VITE_APP_BACKEND_URL || "http://localhost:8080";
+const SOCKET_URL = import.meta.env.VITE_APP_URL || "http://localhost:8080";
 
 let socket;
 
